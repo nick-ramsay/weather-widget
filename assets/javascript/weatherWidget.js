8d55b2a19c3ee02c4eb1546519c819b7c695cfc9
Nick Ramsay <NickRamsay1989@gmail.com>
2019-09-22 15:02:16 +1000

    Five day forecast now working

--- assets/javascript/weatherWidget.js
+++ assets/javascript/weatherWidget.js
@@ -10,6 +10,10 @@ var currentWeather = {
     //iconURL: "http://openweathermap.org/img/wn/" + this.iconID + "@2x.png"
 }
 
+function kelToFahr(kelvin) {
+    return (kelvin - 273.15) * (9 / 5) + 32;
+ }
+
 function getCurrentWeather() {
     var cityID = 2147714;
     
@@ -22,15 +26,16 @@ function getCurrentWeather() {
         currentWeather.cityName = response.name;
         currentWeather.date = response.dt;
         currentWeather.currentTempKel = response.main.temp;
-        currentWeather.currentTempFahr = Math.floor((currentWeather.currentTempKel - 273.15) * (9 / 5) + 32);
+        currentWeather.currentTempFahr = Math.floor(kelToFahr(currentWeather.currentTempKel));
         currentWeather.longDescription = response.weather[0].description;
         currentWeather.shortDescription = response.weather[0].main;
         currentWeather.iconID = response.weather[0].icon;
         currentWeather.iconURL = "http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png";
         calculateForecastDates();
-        renderForecastData();
     })
 }
+
+var daysOfWeek = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 
 var dayOneDate = new Date();
 var dayTwoDate = new Date();
@@ -40,22 +45,27 @@ var dayFiveDate = new Date();
 
 function calculateForecastDates() {
     dayOneDate.setDate(dayOneDate.getDate()+1);
+    dayOneWeather.dayOfWeek = daysOfWeek[dayOneDate.getDay()];
     dayOneDate = dayOneDate.toDateString();
     dayOneWeather.date = dayOneDate;
 
     dayTwoDate.setDate(dayTwoDate.getDate()+2);
+    dayTwoWeather.dayOfWeek = daysOfWeek[dayTwoDate.getDay()];
     dayTwoDate = dayTwoDate.toDateString();
     dayTwoWeather.date = dayTwoDate;
 
     dayThreeDate.setDate(dayThreeDate.getDate()+3);
+    dayThreeWeather.dayOfWeek = daysOfWeek[dayThreeDate.getDay()];
     dayThreeDate = dayThreeDate.toDateString();
     dayThreeWeather.date = dayThreeDate;
 
     dayFourDate.setDate(dayFourDate.getDate()+4);
+    dayFourWeather.dayOfWeek = daysOfWeek[dayFourDate.getDay()];
     dayFourDate = dayFourDate.toDateString();
     dayFourWeather.date = dayFourDate;
 
     dayFiveDate.setDate(dayFiveDate.getDate()+5);
+    dayFiveWeather.dayOfWeek = daysOfWeek[dayFiveDate.getDay()];
     dayFiveDate = dayFiveDate.toDateString();
     dayFiveWeather.date = dayFiveDate;
 }
@@ -63,6 +73,7 @@ function calculateForecastDates() {
 
 var dayOneWeather = {
     date: "",
+    dayOfWeek: "",
     minTemps: [],
     maxTemps: [],
     iconID: "",
@@ -71,6 +82,7 @@ var dayOneWeather = {
 
 var dayTwoWeather = {
     date: "",
+    dayOfWeek: "",
     minTemps: [],
     maxTemps: [],
     iconID: "",
@@ -79,6 +91,7 @@ var dayTwoWeather = {
 
 var dayThreeWeather = {
     date: "",
+    dayOfWeek: "",
     minTemps: [],
     maxTemps: [],
     iconID: "",
@@ -87,6 +100,7 @@ var dayThreeWeather = {
 
 var dayFourWeather = {
     date: "",
+    dayOfWeek: "",
     minTemps: [],
     maxTemps: [],
     iconID: "",
@@ -95,6 +109,7 @@ var dayFourWeather = {
 
 var dayFiveWeather = {
     date: "",
+    dayOfWeek: "",
     minTemps: [],
     maxTemps: [],
     iconID: "",
@@ -109,20 +124,20 @@ function parseForecastData () {
         var forecastDate = new Date(forecastData.list[i].dt * 1000);
         forecastDate = forecastDate.toDateString();
         if (forecastDate === dayOneWeather.date) {
-            dayOneWeather.minTemps.push(forecastData.list[i].main.temp_min);
-            dayOneWeather.maxTemps.push(forecastData.list[i].main.temp_max);
+            dayOneWeather.minTemps.push(Math.floor(kelToFahr(forecastData.list[i].main.temp_min)));
+            dayOneWeather.maxTemps.push(Math.floor(kelToFahr(forecastData.list[i].main.temp_max)));
         } if (forecastDate === dayTwoWeather.date) {
-            dayTwoWeather.minTemps.push(forecastData.list[i].main.temp_min);
-            dayTwoWeather.maxTemps.push(forecastData.list[i].main.temp_max);
+            dayTwoWeather.minTemps.push(Math.floor(kelToFahr(forecastData.list[i].main.temp_min)));
+            dayTwoWeather.maxTemps.push(Math.floor(kelToFahr(forecastData.list[i].main.temp_max)));
         } if (forecastDate === dayThreeWeather.date) {
-            dayThreeWeather.minTemps.push(forecastData.list[i].main.temp_min);
-            dayThreeWeather.maxTemps.push(forecastData.list[i].main.temp_max);
+            dayThreeWeather.minTemps.push(Math.floor(kelToFahr(forecastData.list[i].main.temp_min)));
+            dayThreeWeather.maxTemps.push(Math.floor(kelToFahr(forecastData.list[i].main.temp_max)));
         } if (forecastDate === dayFourWeather.date) {
-            dayFourWeather.minTemps.push(forecastData.list[i].main.temp_min);
-            dayFourWeather.maxTemps.push(forecastData.list[i].main.temp_max);
+            dayFourWeather.minTemps.push(Math.floor(kelToFahr(forecastData.list[i].main.temp_min)));
+            dayFourWeather.maxTemps.push(Math.floor(kelToFahr(forecastData.list[i].main.temp_max)));
         } if (forecastDate === dayFiveWeather.date) {
-            dayFiveWeather.minTemps.push(forecastData.list[i].main.temp_min);
-            dayFiveWeather.maxTemps.push(forecastData.list[i].main.temp_max);
+            dayFiveWeather.minTemps.push(Math.floor(kelToFahr(forecastData.list[i].main.temp_min)));
+            dayFiveWeather.maxTemps.push(Math.floor(kelToFahr(forecastData.list[i].main.temp_max)));
         }  
     }
 }
@@ -138,6 +153,7 @@ function getForecast() {
     }).then(function (response) {
         forecastData = response;
         parseForecastData();
+        renderForecastData();
     })
 }
 
@@ -150,6 +166,26 @@ function renderForecastData() {
     $("#currentWeatherIcon").append(currentWeatherIcon);
     var currentWeatherConditions = $("<p>" + currentWeather.currentTempFahr + "°F</p><p style='padding:0; margin:0;font-size: 12px'>" + currentWeather.shortDescription + "</p>");
     $("#currentWeatherConditions").append(currentWeatherConditions);
+
+    $("#dayOneDow").text(dayOneWeather.dayOfWeek);
+    $("#dayOneLow").text(Math.min.apply(null,dayOneWeather.minTemps) + '°F');
+    $("#dayOneHigh").text(Math.max.apply(null,dayOneWeather.maxTemps) + '°F');
+
+    $("#dayTwoDow").text(dayTwoWeather.dayOfWeek);
+    $("#dayTwoLow").text(Math.min.apply(null,dayTwoWeather.minTemps) + '°F');
+    $("#dayTwoHigh").text(Math.max.apply(null,dayTwoWeather.maxTemps) + '°F');
+
+    $("#dayThreeDow").text(dayThreeWeather.dayOfWeek);
+    $("#dayThreeLow").text(Math.min.apply(null,dayThreeWeather.minTemps) + '°F');
+    $("#dayThreeHigh").text(Math.max.apply(null,dayThreeWeather.maxTemps) + '°F');
+
+    $("#dayFourDow").text(dayFourWeather.dayOfWeek);
+    $("#dayFourLow").text(Math.min.apply(null,dayFourWeather.minTemps) + '°F');
+    $("#dayFourHigh").text(Math.max.apply(null,dayFourWeather.maxTemps) + '°F');
+
+    $("#dayFiveDow").text(dayFiveWeather.dayOfWeek);
+    $("#dayFiveLow").text(Math.min.apply(null,dayFiveWeather.minTemps) + '°F');
+    $("#dayFiveHigh").text(Math.max.apply(null,dayFiveWeather.maxTemps) + '°F');
 
 }
 
